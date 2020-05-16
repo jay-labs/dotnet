0952b9fe6c97fe58afea22dd2c24aadd92485342
jay-labs <jay_nav@outlook.com>
2020-05-17 02:04:21 +0530

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp1.Models;
+namespace WebApp1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult List()
+        {
+            var movie = new Movie() { Id=1, Name = "Titanic" };
+            //return View(movie);
+            //return Content("Hello Movies");
+            return RedirectToAction("Index", "Home", new { param = 1, paramn2 = 2 });
+        }
+
+        
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Models
+{
+    public class Movie
+    {
+        public int Id=0;
+        public string Name ="";
+    }
+}
